5648d14b3022a09a40d83313f6fbb53d9328dd5e
syntat <AKShimalin@gmail.com>
2024-05-06 20:46:14 +0100

    pb components extracted fixed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Filtration from './components/Filtration'
+import PeopleRender from './components/PeopleRender'
+import Adder from './components/Adder'
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [nameFill, setName] = useState('')
+  const [numberFill, setNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
+  const [filteredPersons, setFilteredPersons] = useState(persons)
 
-function App() {
-  const [count, setCount] = useState(0)
+  const nameChange = () => {
+    setName(event.target.value)
+  }  
+  const numberChange = () => {
+    setNumber(event.target.value)
+  }
+  const handleSubmit = () => {
+    event.preventDefault()
+    const newP = {
+      name: nameFill,
+      number: numberFill,
+      id: persons.length + 1,
+    }
+    const modP = persons.concat(newP)
+    setPersons(modP)
+    setFilteredPersons(modP)
+    setName('')
+    setNumber('')
+  }
+
+  const filterChange = () => {
+    setSearchName(event.target.value)
+    const sn = event.target.value
+    console.log(sn)
+    sn === '' 
+    ? setFilteredPersons(persons)
+    : setFilteredPersons(persons.filter((person) => person.name.toLowerCase().includes(sn.toLowerCase())))
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Filtration header='Phonebook' handleChange={filterChange} />
+      <Adder header='add a new' 
+             nameValue = {nameFill} 
+             numberValue = {numberFill} 
+             nameChange={nameChange} 
+             numberChange={numberChange} 
+             handleSubmit={handleSubmit}/>
+      <PeopleRender header = 'Numbers' people={filteredPersons} />  
+    </div>
+  )  
 }
 
 export default App
